6b0928801ce253345760f28e28d08806e3d1f965
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-08-15 17:45:35 +0300

    Local push

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='paroko',
+    version='v.1.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Charles Mwaniki',
+    author_email='mwanikigachanja@gmail.com',
+    description=''
+)
